abfe0cc59838e0536950935cd7fbd05128f77d29
RyanLilla <RyanLilla@users.noreply.github.com>
2023-03-12 22:24:43 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="RyanLilla"
+original_name="my_flask_project"
+original_urlname="my_flask_project"
+original_description="Awesome my_flask_project created by RyanLilla"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv my_flask_project $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
